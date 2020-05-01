93bc18d7a51ccdea5eaf280834074c6e7ce288f9
Miguel Briones <migg.briones@gmail.com>
2020-05-01 17:20:52 -0500

    Se agrega función que obtiene la información de las coordenadas y clima, y archivo README.

---
+++ README.md
@@ -0,0 +1,8 @@
+## Aplicación del Clima - Curso de Node
+
+Recuerden ejecutar ```nm install ```  para las librerías
+
+### Ejemplo:
+```
+node app -d "New York"
+```
--- app.js
+++ app.js
@@ -13,6 +13,24 @@ const argv = require('yargs').options({
 // lugar.getLugarLatLng( argv.direccion )
 //    .then(console.log);
 
-clima.getClima(40.750000, -74.000000)
-    .then( console.log)
-    .catch( console.log );
+// clima.getClima(40.750000, -74.000000)
+//     .then( console.log)
+//     .catch( console.log );
+
+
+const getInfo = async( direccion ) => {
+
+    try {
+        const coords = await lugar.getLugarLatLng( direccion );
+        const temp = await clima.getClima(coords.lat, coords.lng);
+        return `El clima de ${ coords.direccion } es de ${ temp }.`;
+    } catch (error) {
+        return ` No se pudo determinar el clima de ${ direccion }.`;
+    }
+
+    
+}
+
+getInfo ( argv.direccion )
+    .then( console.log )
+    .catch ( console.log );
