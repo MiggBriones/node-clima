40f6b118b0c57a5280232514e911f90ecdc84a5c
Miguel Briones <migg.briones@gmail.com>
2020-05-01 01:09:07 -0500

    Se realiza petición HTTP por medio de axios al API rapidapi

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const axios = require('axios');
+
 const argv = require('yargs').options({
     direccion: {
         alias: 'd',
@@ -7,4 +9,20 @@ const argv = require('yargs').options({
 }).argv;
 
 
-console.log(argv.direccion);
+const encodeUrl = encodeURI( argv.direccion );
+console.log(encodeUrl);
+
+
+// Se crea instancia
+const instance = axios.create({
+    baseURL: `https://devru-latitude-longitude-find-v1.p.rapidapi.com/latlon.php?location=${ encodeUrl }`,
+    headers: {'x-rapidapi-key': '2eae629615mshee66533e685bf95p11d226jsn0bddb51aa149'}
+  });
+
+  instance.get()
+  .then( res => {
+    console.log(res.data.Results[0]);
+  })
+  .catch( err => {
+    console.log('ERORR!!!', err );
+  });
